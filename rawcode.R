05705aa6f356ed6bb6ab9d01068a1cf0d66e4d5b
HornedRat <wiatrak667@op.pl>
2018-03-01 11:50:47 +0100

    Initial commit
    I ignore the unzipped csv file to save space and time

---
+++ rawcode.R
@@ -0,0 +1,12 @@
+library(data.table)
+library(R.utils)
+
+
+setwd("C:\\Users\\jakub.wiatrak\\Desktop\\osobiste\\Data Science\\5. Reproducible Research\\Week 4 - Case studies\\RR-Peer-Assesment-2")
+
+#accessed on 01.03.2018
+download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2", destfile = "StormData.csv.bz2")
+bunzip2("StormData.csv.bz2")
+
+#using fread from data.table to save time (compared to read.csv)
+sd <- fread("StormData.csv")
